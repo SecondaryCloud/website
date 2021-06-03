37243f5e7967ed36782a63a6882c9b11c9518083
VincentHardouin <VincentHardouin@users.noreply.github.com>
2021-06-03 21:01:40 +0000

    deploy: e57dc9318793ab0142970e81126d8d2c0cbc2845

---
+++ _nuxt/static/1622754097/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622754097",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a727a636"}}}}(null,"\u002F"));
