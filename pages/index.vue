e2cc2e1fda8b6c0a0363f8ee9ed2d66654cc0df7
Vincent Hardouin <vincenthardouin@icloud.com>
2021-06-24 11:04:59 +0200

    Add hero banner section

--- pages/index.vue
+++ pages/index.vue
@@ -1,135 +1,89 @@
 <template>
   <div class="index">
     <div class="index__container">
-      <h1 class="index__title">blog-nuxt</h1>
-      <div class="articles">
-        <template v-for="article in articles">
-          <ArticleCard :key="article.slug" :article="article" class="article" />
-        </template>
-      </div>
-      <div class="articles-remaining">
-        <template v-for="article in remainingArticles">
-          <ArticleCard
-            :key="article.slug"
-            :article="article"
-            class="article-remaining"
-          />
-        </template>
-      </div>
+      <section class="index__hero-banner hero-banner">
+        <div class="hero-banner__section">
+          <h1 class="hero-banner__title">
+            Sauvegardez vos données sans même vous en préoccuper
+          </h1>
+          <h2 class="hero-banner__subtitle">
+            Renseigner votre configuration, <br />
+            Secondary se charge de la réplication.
+          </h2>
+          <a class="hero-banner__button button" href="https://app.secondary.fr">
+            Créer ma configuration
+          </a>
+          <span class="hero-banner__information">
+            Secondary est gratuit durant la beta.
+          </span>
+        </div>
+      </section>
     </div>
   </div>
 </template>
 
-<script>
-import ArticleCard from '../components/ArticleCard'
-export default {
-  components: { ArticleCard },
-  async asyncData({ $content }) {
-    const articles = await $content('articles')
-      .only(['title', 'description', 'img', 'slug', 'author'])
-      .sortBy('createdAt', 'asc')
-      .fetch()
-
-    const numberOfArticleChunk = Math.floor(articles.length / 6)
-
-    return numberOfArticleChunk >= 1
-      ? {
-          articles: articles.slice(0, numberOfArticleChunk * 6),
-          remainingArticles: articles.slice(numberOfArticleChunk * 6),
-        }
-      : { remainingArticles: articles }
-  },
-}
-</script>
-
 <style lang="scss">
 .index {
-  background: linear-gradient(
-    170deg,
-    rgba(255, 255, 255, 1) 0%,
-    rgba(220, 246, 246, 0.7) 30%,
-    rgba(200, 241, 241, 0.7) 50%,
-    rgba(178, 235, 235, 0.7) 70%,
-    rgba(62, 214, 221, 0.63) 100%
-  );
-  min-height: 100vh;
-
   &__container {
     margin: 0 auto;
     width: 90%;
 
     @include device-is('tablet') {
-      width: 820px;
+      max-width: 1220px;
     }
   }
 }
 
-.articles {
+.hero-banner {
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
 
   @include device-is('tablet') {
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    grid-auto-rows: 180px 70px 180px;
-    gap: 32px 20px;
-  }
-}
-
-.article {
-  width: 400px;
-  height: 180px;
-  margin: 32px 0 0;
-
-  @include device-is('tablet') {
-    width: 100%;
-    height: 100%;
-    margin: 0;
+    flex-direction: column;
   }
 
-  &:nth-of-type(1n) {
-    grid-column: span 2;
+  &__section {
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+
+    @include device-is('tablet') {
+      width: 50%;
+    }
   }
 
-  &:nth-of-type(2n) {
-    grid-column: span 1;
-    grid-row: span 2;
+  &__title {
+    color: $blumine;
+    font-family: $font-open-sans;
+    font-weight: $font-bold;
+    font-size: 1.625rem;
+    line-height: 2rem;
+
+    @include device-is('tablet') {
+      font-size: 2.75rem;
+      line-height: 3.5rem;
+    }
   }
 
-  &:nth-of-type(3n) {
-    grid-column: span 1;
-    grid-row: span 2;
+  &__subtitle {
+    display: block;
+    margin-top: 21px;
+    color: $blumine;
+    font-family: $font-roboto;
+    font-weight: $font-normal;
+    font-size: 1.625rem;
+    line-height: 2rem;
   }
 
-  &:nth-of-type(6n-1) {
-    grid-column: span 1;
-    grid-row: span 2;
+  &__button {
+    margin-top: 21px;
+    width: 200px;
   }
 
-  &:nth-of-type(6n) {
-    grid-column: span 2;
-    grid-row: span 1;
+  &__information {
+    margin-top: 14px;
+    color: $eden;
+    font-family: $font-roboto;
+    font-size: 0.8125rem;
   }
-}
-
-.articles-remaining {
-  margin: 32px 0;
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  align-items: center;
-  gap: 32px 20px;
-  justify-content: center;
-
-  @include device-is('tablet') {
-    justify-content: left;
-  }
-}
-
-.article-remaining {
-  width: 400px;
-  height: 180px;
 }
 </style>
