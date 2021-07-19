9b3200c2992851fcf26852d2e0f7e79fefc2503c
VincentHardouin <VincentHardouin@users.noreply.github.com>
2021-07-19 06:38:21 +0000

    deploy: f0a486e05ec2cf8cfecfb252d933fe5b5ddbb4b6

--- _nuxt/static/1626676688/tarif/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1626676688",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:null},content:{dbHash:"33b0f196"}}}}("\u002Ftarif"));
